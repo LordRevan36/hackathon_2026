59143ccd10fca1c606c190f281a3148e2a82b766
Thenameofallnames <112591645+Thenameofallnames@users.noreply.github.com>
2026-02-19 22:58:14 -0500

    I'm going to bed

--- player/player.gd
+++ player/player.gd
@@ -18,7 +18,7 @@ var direction
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	global_player.climbEntr.connect(ladderCtrl)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- props/ladder/ladder.gd
+++ props/ladder/ladder.gd
@@ -1,1 +1,4 @@
 extends Area2D
+func _process(delta: float) -> void:
+	if monitoring == true and get_overlapping_bodies()[1] == Player:
+		global_player.climbEntr.emit(self.position.x)
