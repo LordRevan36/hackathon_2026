2abcc263e2d5d58dd071e29615e5fafcc07dee1a
Braedon <bpk07@icloud.com>
2026-02-19 23:57:06 -0500

    Player fixed
    Player has old movement changes restored

--- GlobalScripts/global_player.gd
+++ GlobalScripts/global_player.gd
@@ -3,5 +3,6 @@ extends Node
 #these signals are called as: GlobalPlayer.jumped
 signal landed(collider) #collider is whatever object it collided with
 signal jumped
+signal fellToDeath
 
 signal climbEntr(ladder) #ladder is the ladder emitting
