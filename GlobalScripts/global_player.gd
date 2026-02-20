f30fe9ef252f3c623c23fbb1c6366782b69ce7c3
Thenameofallnames <112591645+Thenameofallnames@users.noreply.github.com>
2026-02-19 21:19:52 -0500

    Slightly unfinished commit (2)

--- GlobalScripts/global_player.gd
+++ GlobalScripts/global_player.gd
@@ -3,3 +3,5 @@ extends Node
 #these signals are called as: GlobalPlayer.jumped
 signal landed(collider) #collider is whatever object it collided with
 signal jumped
+
+signal climbEntr(ladder) #ladder is the ladder emitting
