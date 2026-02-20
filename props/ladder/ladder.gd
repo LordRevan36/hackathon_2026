b7a894285f8643eb1d2c3bf0b62c27c563555fb6
Thenameofallnames <Eze33@icloud.com>
2026-02-20 08:56:19 -0500

    ladder updates

--- props/ladder/ladder.gd
+++ props/ladder/ladder.gd
@@ -1,4 +1,5 @@
 extends Area2D
 func _process(delta: float) -> void:
-	if monitoring == true and get_overlapping_bodies()[1] == Player:
+	if monitoring == true and get_overlapping_bodies()[0] == Player:
+		print("Debug")
 		global_player.climbEntr.emit(self.position.x)
