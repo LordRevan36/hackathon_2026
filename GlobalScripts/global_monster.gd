d2c978826e0adda51e439551fcd3b55e6999ff0e
Braedon <bpk07@icloud.com>
2026-02-21 06:36:11 -0500

    asset updates

--- GlobalScripts/global_monster.gd
+++ GlobalScripts/global_monster.gd
@@ -1,7 +1,7 @@
 extends Node
 
-const MONSTER_WAIT_TIME = 4
+const MONSTER_WAIT_TIME = 6
 const MONSTER_MIN_JUMP_DISTANCE = 20
-const IDEAL_JUMP_HEIGHT = 300
+const IDEAL_JUMP_HEIGHT = 250
 const LANDING_OFFSET = -20 #how many pixels the height of the monster is offset when initially landing on a platform
 const MIN_JUMP_HEIGHT = 100.0 # How high (in pixels) above the highest point (start or end) the arc should go
